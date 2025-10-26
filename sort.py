ff9d19b037bd317becbc10224274d99108bac5af
Zlatomyra Severylova <zseverilova@gmail.com>
2025-10-26 18:47:24 +0200

    Implement sort command functionality and update README with usage details

---
+++ sort.py
@@ -0,0 +1,31 @@
+import click
+
+__version__ = "0.0.1"
+
+@click.command()
+@click.argument('filename', type=click.Path(exists=True))
+@click.option('-r', '--reverse', is_flag=True)
+@click.option('-n', '--numeric', is_flag=True)
+@click.version_option(__version__, '--version')
+def main(filename, reverse, numeric):
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            lines = [line.strip() for line in f]
+
+        if numeric:
+            try:
+                lines = sorted(lines, key=float, reverse=reverse)
+            except ValueError:
+                click.echo("Нечислові дані")
+                return
+        else:
+            lines = sorted(lines, reverse=reverse)
+
+        for line in lines:
+            click.echo(line)
+
+    except Exception:
+        click.echo("Помилка при читанні файлу")
+
+if __name__ == "__main__":
+    main()
